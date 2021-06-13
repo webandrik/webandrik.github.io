07d1301e0319825a0a9213722fcadf52afe04798
webandrik <57045268+webandrik@users.noreply.github.com>
2021-06-13 21:45:02 +0300

    Add files via upload
    vfbccv

---
+++ js/main.js
@@ -0,0 +1,73 @@
+;(function($, undefined){
+  //Begin Main Code
+
+  // Add Events
+  $(window).on('load',function(){
+    $('body').css('overflow', 'auto');
+    $('.loader').fadeOut();
+  });
+  $('.hamburger-btn').on('click', function(event) {
+    $('.hamburger-btn').toggleClass('open');
+    $('body').toggleClass('menu-open');
+    $('.header__nav').toggleClass('is-open');
+  });
+  $('.js-scroll-link').on('click', function(event) {
+    event.preventDefault();
+    var $this_el = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($this_el.attr('href')).offset().top - 0
+    }, 1000);
+  });
+
+  $('#submit').on('click', function(e){
+    e.preventDefault();
+    let f_name = $('#name');
+    let f_email = $('#email');
+    let f_subject = $('#subject');
+    let f_body = $('#body');
+    if(validateInput($('#name')) && validateInput($('#email')) && validateInput($('#subject')) && validateInput($('#body'))) {
+      $.ajax({
+        url: 'sendmail.php',
+        method: 'POST',
+        dataType: 'JSON',
+        data: {
+          name: f_name.val(),
+          email: f_email.val(),
+          subject: f_subject.val(),
+          body: f_body.val()
+        },
+        success: function(response) {
+          $('form-contacts').reset();
+          alert ('Сообщение успешно отправлено!');
+        }
+      });
+    }
+    else {
+      alert('Не заполнены поля.');
+    }
+  });
+
+  // Init Plugins
+
+  AOS.init();
+
+  let mixer = mixitup('.portfolio');
+
+  $('.portfolio').on('mixEnd',function(){
+    if(AOS){ AOS.refresh() }
+  });
+
+  // Functions
+
+  function validateInput(input) {
+    if(input.val().trim()=='') {
+      input.css('border', '1px solid red');
+      return false;
+    }
+    else {
+      input.css('border', '');
+      return true;
+    }
+  }
+
+})(jQuery);
---
+++ sendmail.php
@@ -0,0 +1,47 @@
+<?php 
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
+  
+  require 'PHPMailer/src/Exception.php';
+  require 'PHPMailer/src/PHPMailer.php';
+
+  $mail = new PHPMailer(true);
+  $mail->CharSet = "UTF-8";
+  $mail->setLanguage('ru', 'phpmailer/language/');
+  $mail->IsHTML(true);
+
+  $mail->setFrom('info@site.com', 'Egorov Andrey');
+  $mail->addAddress('admin@site.com');
+
+  $mail->Subject = 'Письмо с формы обратной связи';
+
+  $body = 'Прочтите письмо с сайта (Обратная связь)';
+
+  if(trim(!empty($_POST['name']))) {
+    $body.= '<p>Имя: '.$_POST['name'].'</p>';
+  }
+
+  if(trim(!empty($_POST['email']))) {
+    $body.= '<p>E-mail: '.$_POST['email'].'</p>';
+  }
+
+  if(trim(!empty($_POST['message']))) {
+    $body.= '<p>'.$_POST['message'].'</p>';
+  }
+
+  $mail->Body = $body;
+
+  if($mail->send()) {
+    $res = 'Соообщение успешно отправлено!';
+  }
+
+  else {
+    $res = 'Ошибка при отправке формы:(';
+  }
+
+  $sendResult = ['message' => $res];
+
+  header('Content-type: application/json');
+  echo json_encode($sendResult);
+
+?>
