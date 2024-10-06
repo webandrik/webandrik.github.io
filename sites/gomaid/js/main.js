5b2c9a01021f2ecece5d91a22384e8b47fda56b9
webandrik <57045268+webandrik@users.noreply.github.com>
2024-10-06 19:15:12 +0300

    Add files via upload

---
+++ sites/gomaid/js/main.js
@@ -0,0 +1,27 @@
+const wow = new WOW({
+    animateClass: "animated",
+    offset: 50,
+  });
+wow.init();
+
+const buttonHamburger = document.querySelector(".hamburger"),
+      pageElement = document.querySelector("html"),
+      mainMenu = document.querySelector(".menu__list"),
+      menuLink = document.querySelectorAll(".menu__link");
+
+buttonHamburger.onclick = function () {
+        pageElement.classList.toggle('menu-open');
+        mainMenu.classList.toggle("menu__list--active");
+        buttonHamburger.classList.toggle("hamburger--active");
+}
+
+for (let i = 0; i < menuLink.length; i++) {
+    menuLink[i].addEventListener('click', menuClick);
+}
+
+function menuClick() {
+    console.log('click');
+    pageElement.classList.remove("menu-open");
+    mainMenu.classList.remove("menu__list--active");
+    buttonHamburger.classList.remove("hamburger--active");
+}
