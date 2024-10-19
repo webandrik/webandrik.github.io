2822c4e0416a4ddd5f914a6731422482e5444d97
webandrik <57045268+webandrik@users.noreply.github.com>
2024-10-19 17:47:05 +0300

    Add files via upload

---
+++ sites/curag/js/main.js
@@ -0,0 +1,62 @@
+function menuOpen(el) {
+    
+    const targetEl = $(el).closest('.has-submenu').index();
+
+    $('.menu-list>li').each(function (index, value) {
+        if (index !== targetEl) {
+            $(value).find('.submenu').removeClass('open'); 
+        }
+    });
+
+    $(el).closest('.has-submenu').find('.submenu').toggleClass('open');
+    
+}
+
+$(document).ready(function () {
+    
+    $('#slider-1, #slider-2').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+        {
+        breakpoint: 1400,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+    });
+
+    $(".tours").tabs();
+
+    $("#phone").inputmask({"mask": "+38 (999) 999-99-99"});
+    
+    $('.form-control-select').niceSelect();
+
+    //Обрабатываем мобильное меню
+  
+    $('.header-burger').click(function (event) {
+        $('.has-submenu').append('<span onclick="menuOpen(this)" class="js-submenu"></span>');
+        $('.header-burger, .header-top-wrap').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+
+    $('.menu a').click(function (event) {
+       
+        $('body').removeClass('lock');
+        $('.header-burger, .header-top-wrap').removeClass('active');
+        $(event.target).closest('.submenu').removeClass('open');
+
+    })
+
+});
