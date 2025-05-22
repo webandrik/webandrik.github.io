54f42800173dd98f2b83ea6a4ea60f839f8de018
webandrik <57045268+webandrik@users.noreply.github.com>
2025-05-22 13:58:10 +0300

    Update main.js

--- sites/iwant7/js/main.js
+++ sites/iwant7/js/main.js
@@ -2,8 +2,14 @@
 
 const buttonMenu = document.querySelector(".btn-menu");
 const menu = document.querySelector(".menu");
+
+const btnArrow = document.querySelector(".menu__arrow");
       
 buttonMenu.onclick = function() {
     menu.classList.toggle("menu--active"),
     buttonMenu.classList.toggle("btn-menu--active");
+}
+
+btnArrow.onclick = function() {
+    btnArrow.nextElementSibling.classList.toggle('active');
 }
