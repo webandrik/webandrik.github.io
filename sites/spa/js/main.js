3ee194666a4ed35ab52d15d2d32295963722245d
webandrik <57045268+webandrik@users.noreply.github.com>
2025-07-23 17:16:49 +0300

    Add files via upload

---
+++ sites/spa/js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+  
+  const buttonHamburger = $(".hamburger"),
+        mainMenu = $(".menu"),
+        menuLink = $(".menu__link");
+
+  buttonHamburger.on('click', function () {
+    buttonHamburger.toggleClass("hamburger--active");
+    mainMenu.toggleClass("menu--active");
+  });
+
+  menuLink.on('click', function () {
+    if (buttonHamburger.hasClass('hamburger--active')) {
+      buttonHamburger.removeClass("hamburger--active");
+      mainMenu.removeClass('menu--active');
+    }
+  });
+
+
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        autoplay: true,
+        autoplayHoverPause: true
+    });
+  
+   const toTop = $('.to-top');
+  
+    toTop.hide();  
+
+   $(window).scroll(function () {
+       if ($(this).scrollTop() > 1){  
+         $('.header').addClass("header--sticky");
+         }
+         else{
+         $('.header').removeClass("header--sticky");
+        }
+      if ($(this).scrollTop() > 100) {
+        toTop.fadeIn();
+      }else{
+        toTop.fadeOut();
+      }
+    });
+
+    toTop.click(function () {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 400);
+      return false;
+    });
+		
+  
+});
